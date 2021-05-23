ee102c8e891803345fd814115a75f5ec681b2dcf
Nightstars <christzhangowner#gmail.com>
2021-05-23 20:55:00 +0800

    add refresh token support

--- Idp_mvcClient/Controllers/HomeController.cs
+++ Idp_mvcClient/Controllers/HomeController.cs
@@ -10,6 +10,7 @@ using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -36,7 +37,15 @@ namespace Idp_mvcClient.Controllers
             client.SetBearerToken(accessToken);
             var response = await client.GetAsync("https://localhost:5003/identity");
             if (!response.IsSuccessStatusCode)
+            {
+                if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                {
+                    accessToken = await RenewTokensAsync();
+                    return RedirectToAction();
+                }
                 throw new Exception(response.ReasonPhrase);
+            }
+                
             var content = await response.Content.ReadAsStringAsync();
             return View("Index", content);
         }
@@ -71,6 +80,62 @@ namespace Idp_mvcClient.Controllers
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
             return View("Index");
+        }
+        #endregion
+
+        #region RenewTokensAsync
+        public async Task<string> RenewTokensAsync()
+        {
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+            if (disco.IsError)
+                throw new Exception(disco.Error);
+            var refreshToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.RefreshToken);
+            //refresh access token
+            var tokenResponse =await client.RequestRefreshTokenAsync(new RefreshTokenRequest { 
+                Address=disco.TokenEndpoint,
+                ClientId= "mvc client",
+                ClientSecret= "mvc client",
+                Scope= "scope1 openid profile email phone address",
+                GrantType=OpenIdConnectGrantTypes.RefreshToken,
+                RefreshToken=refreshToken,
+            });
+            if (tokenResponse.IsError)
+                throw new Exception(tokenResponse.Error);
+            else
+            {
+                var expiresAt = DateTime.UtcNow + TimeSpan.FromSeconds(tokenResponse.ExpiresIn);
+                var tokens = new[] {
+                    new AuthenticationToken
+                    {
+                        Name=OpenIdConnectParameterNames.IdToken,
+                        Value=tokenResponse.IdentityToken
+                    },
+                    new AuthenticationToken
+                    {
+                        Name=OpenIdConnectParameterNames.AccessToken,
+                        Value=tokenResponse.AccessToken
+                    },
+                    new AuthenticationToken
+                    {
+                        Name=OpenIdConnectParameterNames.RefreshToken,
+                        Value=tokenResponse.RefreshToken
+                    },
+                    new AuthenticationToken
+                    {
+                        Name="expires_at",
+                        Value=expiresAt.ToString("o",CultureInfo.InvariantCulture)
+                    }
+                };
+                //获取身份认证结果，包含当前Principal和Properties
+                var currentAuthenticationResult = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+                //把新的tokens存起来
+                currentAuthenticationResult.Properties.StoreTokens(tokens);
+                //再登录
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                    currentAuthenticationResult.Principal, currentAuthenticationResult.Properties);
+            }
+            return tokenResponse.AccessToken;
         }
         #endregion
     }
