c1dfd95d409e6a15868411fe60f2bc4ae43fcb29
Nightstars <christzhangowner#gmail.com>
2021-05-23 19:54:18 +0800

    添加项目文件。

---
+++ Idp_mvcClient/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using IdentityModel.Client;
+using Idp_mvcClient.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Idp_mvcClient.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+            if (disco.IsError)
+                throw new Exception(disco.Error);
+            var accessToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
+            client.SetBearerToken(accessToken);
+            var response = await client.GetAsync("https://localhost:5003/identity");
+            if (!response.IsSuccessStatusCode)
+                throw new Exception(response.ReasonPhrase);
+            var content = await response.Content.ReadAsStringAsync();
+            return View("Index", content);
+        }
+
+        public async Task<IActionResult> Privacy()
+        {
+            var accessToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
+            var idToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.IdToken);
+            var refreshToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.RefreshToken);
+            var authorizationCode = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.Code);
+            List<string> data = new List<string>();
+            data.Add($"accessToken,{accessToken}");
+            data.Add($"idToken,{idToken}");
+            data.Add($"refreshToken,{refreshToken}");
+            ViewBag.data = data;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        #region Logout
+        /// <summary>
+        /// Logout
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+            return View("Index");
+        }
+        #endregion
+    }
+}
